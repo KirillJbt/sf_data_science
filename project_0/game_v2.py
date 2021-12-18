ec5282787eedceb43157919126512dbb12a80533
KirillJbt <kirill-maslov1@ya.ru>
2021-12-18 21:47:59 +0300

    edit game_v2

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -1,46 +1,58 @@
+""" GAME  <Guess the number>
+The computer makes its own guess and guesses the number itself
+    """
+
 import numpy as np
 
 def random_predict(number:int=1)->int:
+    """ Randomly guess the number by narrowing the range
+
+    Args:
+        number (int, optional): The hidden number. Defaults to 1.
+
+    Returns:
+        int: Number of tries
+    """
     
     count = 0
+    low_range_limit = 1
+    upper_range_limit = 101
+    predict_number = 50
     
-    while True:
+    while predict_number != number:
         count += 1
-        predict_number = np.random.randint(1, 101) #предполагаемое число
-        if number == predict_number:
-            break # выход из цикла если угадали
-        
+        predict_number = np.random.randint(low_range_limit, upper_range_limit) # estimated number
+        if predict_number > number:
+            upper_range_limit = predict_number
+        elif predict_number < number:
+            low_range_limit = predict_number
+            
     return count
 
 
-
-print(f"Количество попыток: {random_predict(10)}")
-
-
-
-
 def score_game(random_predict) -> int:
-    """За какое количество попыток в среднем за 1000 подходов угадывает наш алгоритм
+    """For how many attempts, on average, 1000 approaches, our algorithm guesses.
 
     Args:
-        random_predict ([type]): функция угадывания
+        random_predict ([type]): guessing function
 
     Returns:
-        int: среднее количество попыток
+        int: average number of attempts
     """
 
-    count_ls = [] # список для сохранения количества попыток
-    np.random.seed(1) # фиксируем сид для вопроизводимости
-    random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
-
+    count_ls = [] # list to save the number of attempts
+    random_array = np.random.randint(1, 101, size=(1000)) # riddle a list of numbers
+    
     for number in random_array:
         count_ls.append(random_predict(number))
 
-    score = int(np.mean(count_ls)) # находим среднее количество попыток
+    score = int(np.mean(count_ls)) # find the average number of attempts
 
-    print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
+    print(f'Your algorithm guesses the number in an average of: {score} tries')
+    
     return(score)
 
-# RUN
-score_game(random_predict)
 
+if __name__ == "__main__":
+    # RUN
+    score_game(random_predict)
